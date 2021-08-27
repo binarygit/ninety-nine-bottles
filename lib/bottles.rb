86b71925f1f22b0be581c09eb4bdd76fcb19e083
binarygit <Inscription.abhi@outlook.com>
2021-08-27 14:17:42 +0545

    create container meth and merge 2 & def case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,16 +15,19 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
         Verse
-    when 2
-       <<-Verse
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-        Verse
     else
         <<-Verse
 #{num} bottles of beer on the wall, #{num} bottles of beer.
-Take one down and pass it around, #{num-1} bottles of beer on the wall.
+Take one down and pass it around, #{num-1} #{container(num-1)} of beer on the wall.
         Verse
+    end
+  end
+
+  def container(number)
+    if number == 1
+     'bottle' 
+    else
+      'bottles'
     end
   end
 
