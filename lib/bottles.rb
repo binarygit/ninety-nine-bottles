2ea11cba882bca40daab3cc18649dc3a5a510f84
binarygit <Inscription.abhi@outlook.com>
2021-08-27 15:59:17 +0545

    create verse with no cases

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,46 +1,6 @@
 #!/usr/bin/ruby
 
-require 'pry-byebug'
-
 class Bottles
-  def verse(num)
-    case num
-    when 0
-      <<-Verse
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-      Verse
-    else
-        <<-Verse
-#{num} #{container(num)} of beer on the wall, #{num} #{container(num)} of beer.
-Take #{pronoun(num)} down and pass it around, #{quantity(num - 1)} #{container(num-1)} of beer on the wall.
-        Verse
-    end
-  end
-
-  def quantity(number = 0)
-    if number == 0
-      'no more'
-    else
-      number
-    end
-  end
-
-  def pronoun(number) 
-    if number == 1
-      'it'
-    else
-      'one'
-    end
-  end
-
-  def container(number)
-    if number == 1
-     'bottle' 
-    else
-      'bottles'
-    end
-  end
 
   def verses(start_from, end_in)
     verses = []
@@ -57,6 +17,53 @@ Take #{pronoun(num)} down and pass it around, #{quantity(num - 1)} #{container(n
 
   def song
     verses(99, 0)
+  end
+
+  def verse(num)
+        <<-Verse
+#{quantity(num).capitalize} #{container(num)} of beer on the wall, #{quantity(num)} #{container(num)} of beer.
+#{action(num)}, #{quantity(sucessor(num))} #{container(sucessor(num))} of beer on the wall.
+        Verse
+  end
+
+  def quantity(number)
+    if number == 0
+      'no more'
+    else
+      number.to_s
+    end
+  end
+
+  def container(number)
+    if number == 1
+     'bottle' 
+    else
+      'bottles'
+    end
+  end
+
+  def action(number)
+    if number == 0
+      'Go to the store and buy some more'
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def pronoun(number) 
+    if number == 1
+      'it'
+    else
+      'one'
+    end
+  end
+
+  def sucessor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
   end
 end
 
